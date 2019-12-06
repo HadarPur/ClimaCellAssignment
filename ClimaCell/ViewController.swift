4fdb80cfb5758f2329d0fa8bc3fcda1eb4c6b202
Hadar Pur <hpur@liveperson.com>
2019-12-06 09:07:55 +0200

    Initial Commit

---
+++ ClimaCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClimaCell
+//
+//  Created by Hadar Pur on 06/12/2019.
+//  Copyright © 2019 Hadar Pur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
